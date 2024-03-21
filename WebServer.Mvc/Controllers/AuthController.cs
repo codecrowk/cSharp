0fa0e8eab5589af07c59dc1d0f89d2494a52ac9f
deiby666 <cardonadeiby88@gmail.com>
2024-03-21 16:05:29 -0500

    Start making MVC

---
+++ WebServer.Mvc/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Mvc.Controllers 
+{
+  public class AuthController : Controller
+  {
+    public IActionResult Login ()
+    {
+      return View();
+    }
+
+    public IActionResult Register ()
+    {
+      return View();
+    }
+  }
+}
